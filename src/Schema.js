fbfe8b550d3c0bf531e20adc248ec3b9fe6fcf7e
codekidX <ashish.shekhar@bookmyshow.com>
2022-11-26 17:36:16 +0530

    schema ui fixes and node ui change

--- src/Schema.js
+++ src/Schema.js
@@ -3,7 +3,7 @@ import { useLocation, useNavigate } from "react-router-dom";
 import "./App.css";
 
 import { invoke } from "@tauri-apps/api/tauri"
-import { Button, ListGroup, ListGroupItem } from "react-bootstrap";
+import { Button, Card, ListGroup, ListGroupItem } from "react-bootstrap";
 import { useEffect, useState } from "react";
 
 function Schema() {
@@ -22,7 +22,7 @@ function Schema() {
 
     return (
         <div className="App">
-            <div className="boxed">{location.state.nickname} :: {location.state.host}</div>
+            <div className="boxed">{location.state.nickname} → {location.state.host}</div>
 
             <div style={{ width: '100%', display: 'flex', flexDirection: 'row-reverse', padding: '1em' }} >
                 <Button style={{ fontSize: '13px' }} variant="danger" onClick={() => {
@@ -35,26 +35,30 @@ function Schema() {
                 }}>Disconnect</Button>
 
             </div>
+            <div>
             {
                 state.nodes.length === 0 ? '' :
-                    (state.nodes.map(n => (
-                        <div>
-                            <div className="boxed" >Node: {n.name}</div>
-                            <ListGroup style={{ borderRadius: '10px', margin: '1em' }}>
-                                <h4>Namespaces</h4>
+                (state.nodes.map(n => (
+                    <Card style={{ backgroundColor: '#141414', color: 'white', margin: '0.5em', borderRadius: '5px' }}>
+                        <Card.Header>
+                        <b style={{ fontFamily: 'monospace' }} >Node: {n.name}</b>
+                        </Card.Header>
+                        <Card.Body>
+                        <ListGroup style={{ borderRadius: '10px', margin: '1em' }}>
                                 {n.namespaces.map((ns, index) => (
                                     <ListGroupItem style={{ color: 'white', backgroundColor: '#242526', border: 'none', cursor: 'pointer' }} onClick={() => {
                                         navigate(`/ns/${ns}`, { state: { ns, node: n.name, host: location.state.host } })
                                     }}>
-                                        ⦿ {ns}
+                                        ↳ {ns}
                                     </ListGroupItem>
                                 ))}
                             </ListGroup>
-
-
-                        </div>
+                        </Card.Body>
+                    </Card>
                     )))
             }
+            </div>
+            
 
         </div>
     )
